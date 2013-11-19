4d591f8d49fbbe489093029fed34d7adc7d9db62
Andrea Giardini <contact@andreagiardini.com>
2013-11-19 15:19:00 +0100

    Es8 - Solved

--- Es1.c
+++ Es1.c
@@ -16,13 +16,13 @@ int main (int argc, char * argv[]){
 
   /* Initialize MPI environment */
   MPI_Init(&argc, &argv) ;
-    
+
   /* Get the size of the MPI_COMM_WORLD communicator */
   MPI_Comm_size(MPI_COMM_WORLD, &nprocs) ;
-   
+
   /* Get my rank... */
   MPI_Comm_rank(MPI_COMM_WORLD, &me) ;
-    
+
   /* ...and print it. */
   printf("\tHello, I am task %d of %d.\n", me, nprocs);
   /* Finalize MPI environment */
